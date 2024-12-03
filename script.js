d0b20af1a365e3139f915b39ddbffd1a6b8afea3
hijeff <jhodgens@gmail.com>
2024-12-02 19:44:39 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Hamburger Menu Toggle
+const hamburger = document.querySelector('.hamburger-menu');
+const navLinks = document.querySelector('.nav-links');
+
+hamburger.addEventListener('click', () => {
+  navLinks.classList.toggle('open');
+  hamburger.classList.toggle('active');
+});
